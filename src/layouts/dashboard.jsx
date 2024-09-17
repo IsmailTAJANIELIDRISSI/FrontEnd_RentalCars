aedc7d750f6c0532e773e65227d9d187214c391e
root <madrid-ismail@mail.ru>
2024-09-17 15:34:34 +0100

    hide footer

--- src/layouts/dashboard.jsx
+++ src/layouts/dashboard.jsx
@@ -52,9 +52,7 @@ export function Dashboard() {
           />
         </Routes>
 
-        <div className="text-blue-gray-600">
-          <Footer />
-        </div>
+        <div className="text-blue-gray-600">{/* <Footer /> */}</div>
       </div>
     </div>
   );
